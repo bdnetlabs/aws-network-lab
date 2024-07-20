1dc6938c6340e741e9db89f1077e2a4935c2e6b2
bdrinkard <briand9475@gmail.com>
2024-07-20 15:23:24 -0400

    Add A record

--- route53.tf
+++ route53.tf
@@ -12,3 +12,10 @@ data "aws_route53_zone" "bdnetlabs" {
 
 # }
 
+resource "aws_route53_record" "ecr-repo" {
+  zone_id = "${data.aws_route53_zone.bdnetlabs.zone_id}"
+  name    = "ecr-repo"
+  type    = "A"
+  ttl     = 300
+  records = [aws_ecrpublic_repository.network_lab_ecr_repo.repository_uri]
+}
