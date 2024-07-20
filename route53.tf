354a5108e36985bf520008ebe488968ec566c859
bdrinkard <briand9475@gmail.com>
2024-07-20 15:31:46 -0400

    Removing because it's not possible

--- route53.tf
+++ route53.tf
@@ -1,21 +1,3 @@
 data "aws_route53_zone" "bdnetlabs" {
   name = "aws.bdnetlabs.com"
 }
-
-# resource "aws_route53_record" "ecr-repo" {
-#   zone_id = "${data.aws_route53_zone.bdnetlabs.zone_id}"
-#   name    = "ecr-repo"
-#   type    = "CNAME"
-#   ttl     = 60
-
-#   records = [aws_ecrpublic_repository.network_lab_ecr_repo.repository_uri]
-
-# }
-
-resource "aws_route53_record" "ecr-repo" {
-  zone_id = "${data.aws_route53_zone.bdnetlabs.zone_id}"
-  name    = "ecr-repo"
-  type    = "A"
-  ttl     = 300
-  records = [aws_ecrpublic_repository.network_lab_ecr_repo.repository_uri]
-}
