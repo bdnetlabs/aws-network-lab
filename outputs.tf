4e910b874a457d32e923ca9187564e5ed276ff19
bdrinkard <briand9475@gmail.com>
2024-07-20 01:05:02 -0400

    Add subnet IDs to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "private_subnet_list" {
   description = "The ARNs of the private subnets"
-  value = module.network_lab_vpc.private_subnet_arns
+  value       = module.network_lab_vpc.private_subnet_arns
 }
 
 output "public_subnet_list" {
   description = "The ARNs of the public subnets"
-  value = module.network_lab_vpc.public_subnet_arns
+  value       = module.network_lab_vpc.public_subnet_arns
 }
