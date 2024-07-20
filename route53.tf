f3d98cb7b1d6bf3281534ee0c40033665ed5b88f
bdrinkard <briand9475@gmail.com>
2024-07-20 15:12:21 -0400

    Add CNAME record for ecr-repo

--- route53.tf
+++ route53.tf
@@ -1,3 +1,12 @@
 data "aws_route53_zone" "bdnetlabs" {
-    name = "aws.bdnetlabs.com"
+  name = "aws.bdnetlabs.com"
+}
+
+resource "aws_route53_record" "ecr-repo" {
+  zone_id = "${data.aws_route53_zone.bdnetlabs.zone_id}"
+  name    = "ecr-repo"
+  type    = "CNAME"
+
+  records = [aws_ecrpublic_repository.network_lab_ecr_repo.repository_uri]
+
 }
