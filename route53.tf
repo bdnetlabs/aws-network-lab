c8283f9609dbd7e224152b93c339e14894c24111
bdrinkard <briand9475@gmail.com>
2024-07-20 15:15:42 -0400

    Adding TTL

--- route53.tf
+++ route53.tf
@@ -6,6 +6,7 @@ resource "aws_route53_record" "ecr-repo" {
   zone_id = "${data.aws_route53_zone.bdnetlabs.zone_id}"
   name    = "ecr-repo"
   type    = "CNAME"
+  ttl     = 60
 
   records = [aws_ecrpublic_repository.network_lab_ecr_repo.repository_uri]
 
