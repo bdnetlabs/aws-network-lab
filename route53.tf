348d4a071245913dbe34d4845453058c98e74b04
bdrinkard <briand9475@gmail.com>
2024-07-20 15:23:16 -0400

    comment out non-working config

--- route53.tf
+++ route53.tf
@@ -2,12 +2,13 @@ data "aws_route53_zone" "bdnetlabs" {
   name = "aws.bdnetlabs.com"
 }
 
-resource "aws_route53_record" "ecr-repo" {
-  zone_id = "${data.aws_route53_zone.bdnetlabs.zone_id}"
-  name    = "ecr-repo"
-  type    = "CNAME"
-  ttl     = 60
+# resource "aws_route53_record" "ecr-repo" {
+#   zone_id = "${data.aws_route53_zone.bdnetlabs.zone_id}"
+#   name    = "ecr-repo"
+#   type    = "CNAME"
+#   ttl     = 60
 
-  records = [aws_ecrpublic_repository.network_lab_ecr_repo.repository_uri]
+#   records = [aws_ecrpublic_repository.network_lab_ecr_repo.repository_uri]
 
-}
+# }
+
