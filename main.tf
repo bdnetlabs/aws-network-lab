6cdd8230adcb1026ac829afc5023993d2fc1dd2f
bdrinkard <briand9475@gmail.com>
2024-07-20 00:35:40 -0400

    Removed Ansible Instance

--- main.tf
+++ main.tf
@@ -12,9 +12,9 @@ provider "aws" {
 
 
 ##### CREATE VPC FOR NETWORK AUTOMATION #####
-module "netautomation_vpc" {
+module "network_lab_vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.2.0"
+  version = "${var.vpc_module_version}"
 
   name = var.vpc_name
   cidr = var.vpc_cidr
@@ -26,19 +26,4 @@ module "netautomation_vpc" {
   enable_nat_gateway = var.vpc_enable_nat_gateway
 
   tags = var.vpc_tags
-}
-
-module "ansible-instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "5.5.0"
-  count   = 1
-
-  name = "ansible"
-
-  ami                         = "ami-0f79abea6c14ac085"
-  instance_type               = var.ec2_instance_type
-  vpc_security_group_ids      = [module.netautomation_vpc.default_security_group_id]
-  subnet_id                   = module.netautomation_vpc.public_subnets[0]
-  associate_public_ip_address = true
-  key_name                    = "automation-server-key"
 }
