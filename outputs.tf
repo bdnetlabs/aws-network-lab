7910bafda74657c0297395a6e3b19c124c5b6efa
bdrinkard <briand9475@gmail.com>
2024-07-20 01:29:01 -0400

    Output the ecr repository URI

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "public_subnet_list" {
   description = "The ARNs of the public subnets"
   value       = module.network_lab_vpc.public_subnet_arns
 }
+
+output "network_lab_ecr_uri" {
+  description = "The URI for the ECR Repository"
+  value = aws_ecrpublic_repository.network_lab_ecr_repo.URI
+}
